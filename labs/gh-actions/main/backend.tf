13b9110facc753db4d43783916633df74ae80996
TEK Student <TEKstudent@vlab.lan>
2024-12-12 15:14:38 -0700

    Update S3 backend configuration -Tiru

--- labs/gh-actions/main/backend.tf
+++ labs/gh-actions/main/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-state-bucket-antoine-victor "
+    bucket = "tirudarocks-099"
     key    = "terraform.tfstate"
     region = "us-west-1"
   }
